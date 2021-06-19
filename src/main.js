9026a6b1eb36de8cf7d2ce2028bfc88908d8429a
rockysaikia730 <67681828+rockysaikia730@users.noreply.github.com>
2021-06-19 22:44:42 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import langvalue from './mixins/langvalue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mixin(langvalue).mount('#app')
