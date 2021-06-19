f9dc059340e955fcdc94b14c8d697985898d10a7
rockysaikia730 <67681828+rockysaikia730@users.noreply.github.com>
2021-06-19 23:06:07 +0530

    Update index.js
    Language_state added

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,29 @@
 import { createStore } from 'vuex'
+import message_en from '../locales/en.json'
+import message_as from '../locales/as.json'
+import message_hn from '../locales/hn.json'
+import message_bn from '../locales/bn.json'
 
 export default createStore({
   state: {
+    lang: message_en
+   
   },
   mutations: {
+    changelang(state, lang_val) {
+      if (lang_val === 'en') {
+        state.lang = message_en
+      }
+      else if (lang_val === 'as') {
+        state.lang = message_as
+      }
+      else if (lang_val === 'bn') {
+        state.lang = message_bn
+      }
+      else if (lang_val === 'hn') {
+        state.lang = message_hn
+      }
+   }
   },
   actions: {
   },
